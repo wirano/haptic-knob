3dac6545858d335cd96186815339b3a63ad9e82a
wirano <git@wirano.me>
2023-05-01 18:42:58 +0800

    use simpleFOC's pid

--- Firmware/haptic-knob/components/foc/include/pid.h
+++ Firmware/haptic-knob/components/foc/include/pid.h
@@ -1,68 +1,51 @@
-// MIT License
+//MIT License
 //
-// Copyright (c) 2020-2023 wirano
+//Copyright (c) 2020 Antun Skuric
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 
-//
-// Created by wirano on 20-5-17.
-//
 
 #ifndef PID_H
 #define PID_H
 
+
 #include <stdint.h>
 
-typedef struct {
-    float Kp;
-    float Ki;
-    float Kd;
-    float Ki_separation;
-
-    float err;
-    float err_prev;
-    float err_last;
-    uint32_t time_prev;
-
-    uint32_t (*get_micros)(void);
-} pid_incremental_t;
 
 typedef struct {
-    float Kp;
-    float Ki;
-    float Kd;
-    float Ki_Limit;
-    float Ki_Separation;
+    float P; //!< Proportional gain
+    float I; //!< Integral gain
+    float D; //!< Derivative gain
+    float output_ramp; //!< Maximum speed of change of the output value
+    float limit; //!< Maximum output value
 
-    float err;
-    float err_prev;
-    float integral;
-    uint32_t time_prev;
+    float error_prev; //!< last tracking error value
+    float output_prev;  //!< last pid output value
+    float integral_prev; //!< last integral component value
+    unsigned long timestamp_prev; //!< Last execution timestamp
 
     uint32_t (*get_micros)(void);
-} pid_positional_t;
+} pid_controller_t;
 
 
-float PID_IncrementalRealize(pid_incremental_t *PID, float inVal, float target);
+float pid_calc(pid_controller_t *pid, float input, float target);
 
-float PID_PositionalRealize(pid_positional_t *PID, float inVal, float target);
-
+void pid_reset(pid_controller_t *pid);
 
 #endif //PID_H
--- Firmware/haptic-knob/components/foc/pid.c
+++ Firmware/haptic-knob/components/foc/pid.c
@@ -1,88 +1,77 @@
-// MIT License
+//MIT License
 //
-// Copyright (c) 2020-2023 wirano
+//Copyright (c) 2020 Antun Skuric
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 
-//
-// Created by wirano on 20-5-17.
-//
 
 #include "pid.h"
 #include <math.h>
+#include "foc_utils.h"
 
 
-float PID_IncrementalRealize(pid_incremental_t *PID, float inVal, float target) {
-    uint8_t Ki_enable = 1;
-    uint32_t time_now = PID->get_micros();
-    float Ts = (float) (time_now - PID->time_prev) * 1e-6f;
-    if (Ts <= 0 || Ts > 0.5) Ts = 1e-6f;
+float pid_calc(pid_controller_t *pid, float input, float target) {
+    float error = target - input;
+    // calculate the time from the last call
+    unsigned long timestamp_now = pid->get_micros();
+    float Ts = (timestamp_now - pid->timestamp_prev) * 1e-6f;
+    // quick fix for strange cases (micros overflow)
+    if(Ts <= 0 || Ts > 0.5f) Ts = 1e-3f;
 
-    PID->err = target - inVal;
+    // u(s) = (P + I/s + Ds)e(s)
+    // Discrete implementations
+    // proportional part
+    // u_p  = P *e(k)
+    float proportional = pid->P * error;
+    // Tustin transform of the integral part
+    // u_ik = u_ik_1  + I*Ts/2*(ek + ek_1)
+    float integral = pid->integral_prev + pid->I*Ts*0.5f*(error + pid->error_prev);
+    // antiwindup - limit the output
+    integral = _limit(integral, -pid->limit, pid->limit);
+    // Discrete derivation
+    // u_dk = D(ek - ek_1)/Ts
+    float derivative = pid->D*(error - pid->error_prev)/Ts;
 
-    if (fabsf(PID->err) > PID->Ki_separation) Ki_enable = 0;
-
-    float proportional = PID->Kp * (PID->err - PID->err_prev);
-
-    float integral = 0;
-    if (Ki_enable) integral = PID->Ki * Ts * PID->err;
-
-    float derivative = PID->Kd * (PID->err - 2 * PID->err_prev + PID->err_last) / Ts;
-
-    float increment = proportional + integral + derivative;
-
-    PID->err_last = PID->err_prev;
-    PID->err_prev = PID->err;
-
-    return increment;
-}
-
-float PID_PositionalRealize(pid_positional_t *PID, float inVal, float target) {
-    uint32_t time_now = PID->get_micros();
-    float Ts = (float) (time_now - PID->time_prev) * 1e-6f;
-    if (Ts <= 0 || Ts > 0.5) Ts = 1e-3f;
-
-    PID->err = target - inVal;
-//    if (PID->err < PID->Ki_Separation && PID->err > -PID->Ki_Separation)
-//        PID->integral += PID->err;
-//    else
-//        PID->integral = PID->integral;
-
-    if (PID->integral > PID->Ki_Limit)
-        PID->integral = PID->Ki_Limit;
-    else if (PID->integral < -PID->Ki_Limit)
-        PID->integral = -PID->Ki_Limit;
-
-
-    float proportional = PID->Kp * PID->err;
-
-    float integral_new = PID->integral + (PID->err + PID->err_prev) / 2.f;
-    float integral = PID->Ki * Ts * integral_new;
-
-    float derivative = PID->Kd * (PID->err - PID->err_prev) / Ts;
-
+    // sum all the components
     float output = proportional + integral + derivative;
+    // antiwindup - limit the output variable
+    output = _limit(output, -pid->limit, pid->limit);
 
-    PID->err_prev = PID->err;
-    PID->integral = integral_new;
-
+    // if output ramp defined
+    if(pid->output_ramp > 0){
+        // limit the acceleration by ramping the output
+        float output_rate = (output - pid->output_prev)/Ts;
+        if (output_rate > pid->output_ramp)
+            output = pid->output_prev + pid->output_ramp*Ts;
+        else if (output_rate < -pid->output_ramp)
+            output = pid->output_prev - pid->output_ramp*Ts;
+    }
+    // saving for the next pass
+    pid->integral_prev = integral;
+    pid->output_prev = output;
+    pid->error_prev = error;
+    pid->timestamp_prev = timestamp_now;
     return output;
 }
 
+void pid_reset(pid_controller_t *pid){
+    pid->integral_prev = 0;
+    pid->error_prev = 0;
+    pid->output_prev = 0;
+}
