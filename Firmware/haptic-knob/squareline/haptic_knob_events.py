1563e1127cdbf26824c12d09708fa023ec0a2b97
wirano <git@wirano.me>
2023-05-04 21:27:49 +0800

    add squareline ui

---
+++ Firmware/haptic-knob/squareline/haptic_knob_events.py
@@ -0,0 +1,16 @@
+
+def init_action(event_struct):
+    return
+
+
+def init_action1(event_struct):
+    return
+
+
+def update_lable(event_struct):
+    return
+
+
+def select_mode(event_struct):
+    return
+
